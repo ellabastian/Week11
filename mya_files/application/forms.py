c8ee87b1864ff94ae55b4f2974a741d0c7dc880a
Mya Miah <mya.miah1980@gmail.com>
2022-04-10 16:09:29 +0100

    Updated files after end of 2nd session with Ella

---
+++ mya_files/application/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, DateField, SelectField
+
+class BasicForm(FlaskForm):
+    first_name = StringField("First Name")
+    email = StringField("Email")
+    date_of_birth = DateField("Date of Birth")
+    favourite_character = SelectField("Favourite Character", choices=['Geralt', 'Yennefer', 'Ciri', 'Dandelion aka The Bard', 'Fringilla Vigo', 'Triss', 'Tissai', 'Cahir', 'Istredd', 'Vesemir'])
+    submit = SubmitField("Add Details")
